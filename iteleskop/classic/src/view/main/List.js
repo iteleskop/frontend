5522545800de7d8e2baa7170695a0aa7ecf2c05f
Tomek Mrugalski <tomasz.mrugalski@gmail.com>
2015-11-30 00:49:31 +0100

    Spora czesc kolumn w podgladzie zadan jest teraz niewidoczna by default

--- iteleskop/classic/src/view/main/List.js
+++ iteleskop/classic/src/view/main/List.js
@@ -24,7 +24,11 @@ Ext.define('iTeleskop.view.main.Tasks', {
               return RAfloatToHMS(value);
           }
         },
-        { text: 'Dec', dataIndex: 'decl', flex: 1 },
+        { text: 'Dec', dataIndex: 'decl', flex: 1,
+          renderer: function(value) {
+              return DeclinationFloatToDMS(value);
+          }
+        },
         { text: 'Opis', dataIndex: 'descr', flex: 1 },
         { text: 'Komentarz', dataIndex: 'comment', flex: 1 },
         { text: 'Filtr', dataIndex: 'filter', flex: 1 },
@@ -35,19 +39,27 @@ Ext.define('iTeleskop.view.main.Tasks', {
         { text: 'VPhot', dataIndex: 'vphot', flex: 1 },
 
         /* ograniczenia */
-        { text: 'Min. wysokosc', dataIndex: 'min_alt', flex: 1 },
-        { text: 'Min. old od Księżyca', dataIndex: 'moon_distance', flex: 1 },
-        { text: 'Nie wcześniej niż', dataIndex: 'skip_before', flex: 1 },
-        { text: 'Nie później niż', dataIndex: 'skip_after', flex: 1 },
-        { text: 'Nie częściej niż sekund', dataIndex: 'skip_interval', flex: 1 },
-        { text: 'Omiń, jeżeli w ciągu X sekund...', dataIndex: 'skip_period_seconds', flex: 1 },
-        { text: '... było Y obserawacji', dataIndex: 'skip_period_count', flex: 1 }
+        { text: 'Min. wysokosc', dataIndex: 'min_alt', flex: 1,
+          hidden: true, hideable: true },
+        { text: 'Min. old od Księżyca', dataIndex: 'moon_distance', flex: 1,
+          hidden: true, hideable: true },
+        { text: 'Nie wcześniej niż', dataIndex: 'skip_before', flex: 1,
+          hidden: true, hideable: true },
+        { text: 'Nie później niż', dataIndex: 'skip_after', flex: 1,
+          hidden: true, hideable: true },
+        { text: 'Nie częściej niż sekund', dataIndex: 'skip_interval', flex: 1,
+          hidden: true, hideable: true },
+        { text: 'Omiń, jeżeli w ciągu X sekund...', dataIndex: 'skip_period_seconds', flex: 1,
+          hidden: true, hideable: true },
+        { text: '... było Y obserawacji', dataIndex: 'skip_period_count', flex: 1,
+          hidden: true, hideable: true},
 
-/* Tych pol nie pokazujemy, bo jest za duza kaszana
-        { text: 'Dodatkowe komendy', dataIndex: 'other_cmd', flex: 1 },
-        { text: 'Plik', dataIndex: 'imagename', flex: 1 }
+        /* pozostale */
+        { text: 'Dodatkowe komendy', dataIndex: 'other_cmd', flex: 1,
+          hidden: true, hideable: true },
+        { text: 'Plik', dataIndex: 'imagename', flex: 1,
+          hidden: true, hideable: true }
 
- */
     ],
 
     listeners: {
