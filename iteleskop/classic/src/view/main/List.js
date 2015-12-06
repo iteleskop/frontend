8cf138bfe0456b0b1045e4892f8362188800a10d
Tomek Mrugalski <tomasz.mrugalski@gmail.com>
2015-12-06 19:50:01 +0100

    Scrollowanie listy taskow, wyswietlane od najnowszych

--- iteleskop/classic/src/view/main/List.js
+++ iteleskop/classic/src/view/main/List.js
@@ -15,6 +15,9 @@ Ext.define('iTeleskop.view.main.Tasks', {
         type: 'tasks'
     },
 
+    autoScroll: true,
+    height: 900,
+
     columns: [
         { text: 'Task ID',  dataIndex: 'task_id', width: 70 },
         { text: 'Stan', dataIndex: 'state' },
